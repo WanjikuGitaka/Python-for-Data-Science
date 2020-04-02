daa3ea8fc27785d1462a3f4073313c7cdb6eb510
Gladwell Gitaka <56022415+WanjikuGitaka@users.noreply.github.com>
2020-04-02 13:50:20 +0300

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,1 +1,37 @@
+# Creating an artist summary function
+def artist_summary(artist):
+    num_artworks = artist_freq[artist]
+    template = "There are {num} artworks by {name} in the data set"
+    output = template.format(name=artist, num=num_artworks)
+    print(output)
 
+artist_summary("Henri Matisse")
+print(artist_summary)
+# Formatting numbers inside strings
+pop_millions = [
+    ["China", 1379.302771],
+    ["India", 1281.935991],
+    ["USA",  326.625791],
+    ["Indonesia",  260.580739],
+    ["Brazil",  207.353391],
+]
+
+template = "The population of {} is {:,.2f} million"
+for country in pop_millions:
+    name = country[0]
+    pop = country[1]
+    
+    output = template.format(name, pop)
+    print(output) 
+# Summarizing gender artwork data
+ender_freq = {}
+for row in moma:
+    gender = row[5]
+    if gender not in gender_freq:
+        gender_freq[gender] = 1
+    else:
+        gender_freq[gender] += 1
+        
+for gender, num in gender_freq.items():
+    template = "There are {n:,} artworks by {g} artists"
+    print(template.format(g=gender, n=num))
